a137a284e8d0b41fb979d3788aba060769c2601a
yonathangashu <yonathangashu@gmail.com>
2026-09-02 17:19:31 -0400

    Opaque bg, lualine symbols removed, yabai typo fix

--- .config/nvim/lua/plugins/lualine.lua
+++ .config/nvim/lua/plugins/lualine.lua
@@ -12,7 +12,7 @@ return {
     local filename = {
       'filename',
       file_status = true, -- displays file status (readonly status, modified status)
-      path = 0, -- 0 = just filename, 1 = relative path, 2 = absolute path
+      path = 1, -- 0 = just filename, 1 = relative path, 2 = absolute path
     }
 
     local hide_in_width = function()
@@ -44,7 +44,7 @@ return {
         -- Some useful glyphs:
         -- https://www.nerdfonts.com/cheat-sheet
         --        
-        section_separators = { left = ' ', right = '' },
+        section_separators = { left = ' ', right = ' ' },
         component_separators = { left = '', right = '' },
         disabled_filetypes = { 'alpha', 'neo-tree' },
         always_divide_middle = true,
